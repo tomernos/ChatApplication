1faf0aa5af9c2fe79621e1079ecb753c4ec6f947
tomernos <tomernos1@gmail.com>
2024-08-24 16:43:00 +0300

    changes test

--- test_myflask.py
+++ test_myflask.py
@@ -15,15 +15,3 @@ def test_login_page(client):
     response = client.get('/login')
     assert response.status_code == 200
     assert b"Login" in response.data
-import pytest
-from my_flask import app
-
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
-
-def test_home_page(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Welcome to the Flask App" in response.data
